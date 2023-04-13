8f43328a1404f49af49fa9ddb95ff137e2afcf45
Alan.Chu <achu61@seas.upenn.edu>
2023-04-12 18:01:36 -0700

    initail fetch API

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('', views.about, name='about'),
+    path('fetch_pokemons/', views.fetch_pokemons, name='fetch_pokemons'),
     path('pokemons/', views.pokemons_view, name='pokemons_view'),
     path('pokemons/<int:pokemon_id>/',
          views.pokemon_detail, name='pokemon_detail'),
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.shortcuts import render
 from .models import Pokemon
 
@@ -17,4 +18,19 @@ def pokemons_view(request):
 
 def pokemon_detail(request, pokemon_id):
     pokemon = Pokemon.objects.get(id=pokemon_id)
-    return render(request, 'pokemons/pokemon_detail.html', {'pokemon': pokemon})
+    return render(request, 'pokemons/pokemon_detail.html', {'pokemon': pokemon, 'title': 'Pokemon Detail'})
+
+
+def fetch_pokemons(request):
+    pokemons = []
+    for i in range(1, 10):
+        url = f'https://pokeapi.co/api/v2/pokemon/{i}'
+        response = requests.get(url)
+        data = response.json()
+        pokemon = {
+            'name': data['forms'][0]['name'].capitalize(),
+            'id': data['id'],
+            'img': data['sprites']['other']['official-artwork']['front_default'],
+        }
+        pokemons.append(pokemon)
+    return render(request, 'pokemons/fetch_pokemons.html', {'pokemons': pokemons, 'title': 'Fetch Pokemons'})
