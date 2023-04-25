a851813fe31218d2e73c7e47bdfbc3fd5a4e4b73
Alan.Chu <achu61@seas.upenn.edu>
2023-04-25 16:53:03 -0700

    css bugs +session

--- main_app/views.py
+++ main_app/views.py
@@ -9,17 +9,17 @@ from django.contrib.auth.decorators import login_required
 
 
 def home(request):
-    return render(request, 'home.html', {'title': 'HomePage'})
+    return render(request, "home.html", {"title": "HomePage"})
 
 
 def about(request):
-    return render(request, 'about.html', {'title': 'AboutPage'})
+    return render(request, "about.html", {"title": "AboutPage"})
 
 
 @login_required
 def pokemons_view(request):
     pokemons = Pokemon.objects.filter(ownedby=request.user)
-    return render(request, 'pokemons/pokemons_view.html', {'pokemons': pokemons, 'title': 'View Your Pokemons'})
+    return render(request, "pokemons/pokemons_view.html", {"pokemons": pokemons, "title": "View Your Pokemons"})
 
 
 @login_required
@@ -27,58 +27,64 @@ def pokemon_detail(request, pokemon_id):
     feeding_form = FeedingForm()
     pokemon = Pokemon.objects.get(id=pokemon_id)
     check_items_evolve(request, pokemon_id)
-    print(pokemon.evolve_chains)
-    return render(request, 'pokemons/pokemon_detail.html', {'pokemon': pokemon, 'title': 'Pokemon Detail', 'user': request.user, 'feeding_form': feeding_form})
+    # print(pokemon.evolve_chains)
+    return render(request, "pokemons/pokemon_detail.html", {"pokemon": pokemon, "title": "Pokemon Detail", "user": request.user, "feeding_form": feeding_form})
 
 
 @login_required
 def level_up(request, pokemon_id):
     pokemon = Pokemon.objects.get(id=pokemon_id)
-    if request.method == 'POST':
+    if request.method == "POST":
         pokemon.level_up()
-        return redirect('pokemon_detail', pokemon_id=pokemon.id)
+        return redirect("pokemon_detail", pokemon_id=pokemon.id)
 
 
 def pokemon_pocket_box(request, pokemon_id):
     pokemon = Pokemon.objects.get(id=pokemon_id)
     # print(pokemon.name)
     # print(pokemon.in_pocket)
-    if request.method == 'POST':
+    if request.method == "POST":
         if pokemon.in_pocket:
             pokemon.in_pocket = False
         else:
             pokemon.in_pocket = True
         pokemon.save()
-        return redirect('pokemons_view')
+        return redirect("pokemons_view")
 
 
 def check_evolve(pokemon_id):
-    url = f'https://pokeapi.co/api/v2/pokemon-species/{pokemon_id}'
+    url = f"https://pokeapi.co/api/v2/pokemon-species/{pokemon_id}"
     response = requests.get(url)
-    data = response.json().get('evolution_chain').get('url')
+    data = response.json().get("evolution_chain").get("url")
     data = requests.get(data).json()
     item = None
-    min_level = data['chain']['evolves_to'][0]['evolution_details'][0]['min_level']
+    min_level = data["chain"]["evolves_to"][0]["evolution_details"][0]["min_level"]
     if min_level != None:
-        evole_to = data['chain']['evolves_to'][0]['species']['name']
+        evole_to = data["chain"]["evolves_to"][0]["species"]["name"]
     else:
-        evole_to = data['chain']['evolves_to'][0]['evolves_to'][0]['species']['name']
-        item = data['chain']['evolves_to'][0]['evolves_to'][0]['evolution_details'][0]['item']['name']
-
-    print(min_level, evole_to, item)
-    return {'evole_to': evole_to, 'min_level': min_level, 'item': item}
+        evolves_to = data['chain']['evolves_to']
+        evole_to = evolves_to[0]['species']['name'] if evolves_to else None
+        min_level = evolves_to[0]['evolution_details'][0]['min_level'] if evolves_to else None
+        item = None
+    for detail in evolves_to[0]['evolution_details']:
+        if 'item' in detail:
+            item = detail['item']['name']
+            break
+    # print(evole_to, item)
+    # print(min_level, evole_to, item)
+    return {"evole_to": evole_to, "min_level": min_level, "item": item}
 
 
 def check_items_evolve(request, pokemon_id):
     pokemon = Pokemon.objects.get(id=pokemon_id)
     player = Player.objects.get(ownedby=request.user)
-    required_evolve_item = pokemon.evolve_chains['item']
-    print(list(player.items.keys()))
+    required_evolve_item = pokemon.evolve_chains["item"]
+    # print(list(player.items.keys()))
     if required_evolve_item in list(player.items.keys()):
         pokemon.ready_to_evolve = True
         pokemon.save()
         # print(
-        #     f"{pokemon} is ready to evolve to {pokemon.evolve_chains['evole_to']}")
+        #     f"{pokemon} is ready to evolve to {pokemon.evolve_chains["evole_to"]}")
         return True
     else:
         return False
@@ -87,66 +93,71 @@ def check_items_evolve(request, pokemon_id):
 def evolve_pokemon(request, pokemon_id):
     pokemon = Pokemon.objects.get(id=pokemon_id)
     player = Player.objects.get(ownedby=request.user)
-    required_item = pokemon.evolve_chains.get('item')
+    required_item = pokemon.evolve_chains.get("item")
     if pokemon.ready_to_evolve:
         if player.items.get(required_item) == 1:
             del player.items[required_item]
         else:
             player.items[request.post.get(required_item)] -= 1
-        pokemon.evolve(pokemon.evolve_chains['evole_to'])
+        pokemon.evolve(pokemon.evolve_chains["evole_to"])
         pokemon.save()
         player.save()
-    return redirect('pokemon_detail', pokemon_id=pokemon.id)
+    return redirect("pokemon_detail", pokemon_id=pokemon.id)
 
 
 def capture_pokemon(request, pokemon_id):
-    capture_pokemon_id = request.POST.get('pokemon_id')
+    capture_pokemon_id = request.POST.get("pokemon_id")
     captured_pokemon = fetch_pokemon(capture_pokemon_id)
-    name = captured_pokemon['name']
+    name = captured_pokemon["name"]
     level = 5
-    img = captured_pokemon['img']
+    img = captured_pokemon["img"]
     evolve_chains = check_evolve(capture_pokemon_id)
     ownedby = request.user
     in_pocket = True if len(
         Pokemon.objects.filter(in_pocket=True)) < 6 else False
     new_pokemon = CapturePokemonForm(
-        {'pokemon_id': capture_pokemon_id, 'name': name, 'level': level, 'img': img, 'ownedby': ownedby,  'in_pocket': in_pocket, "evolve_chains": evolve_chains})
+        {"pokemon_id": capture_pokemon_id, "name": name, "level": level, "img": img, "ownedby": ownedby,  "in_pocket": in_pocket, "evolve_chains": evolve_chains})
     new_pokemon.save()
-    return redirect('/pokemons/')
+    return redirect("/pokemons/")
 
 
 class PokemonCreate(CreateView):
     model = Pokemon
-    fields = '__all__'
-    success_url = '/pokemons/'
-    template_name = 'pokemons/pokemon_form.html'
+    fields = "__all__"
+    success_url = "/pokemons/"
+    template_name = "pokemons/pokemon_form.html"
 
 
 class PokemonRelease(DeleteView):
     model = Pokemon
-    success_url = '/pokemons/'
-    template_name = 'pokemons/pokemon_confirm_delete.html'
+    success_url = "/pokemons/"
+    template_name = "pokemons/pokemon_confirm_delete.html"
 
 
 def fetch_pokemon(id):
-    url = f'https://pokeapi.co/api/v2/pokemon/{id}'
+    url = f"https://pokeapi.co/api/v2/pokemon/{id}"
     response = requests.get(url)
     data = response.json()
     pokemon = {
-        'name': data['forms'][0]['name'].capitalize(),
-        'id': data['id'],
-        'img': data['sprites']['other']['official-artwork']['front_default'],
-        # 'moves': data['moves'][0],
+        "name": data["forms"][0]["name"].capitalize(),
+        "id": data["id"],
+        "img": data["sprites"]["other"]["official-artwork"]["front_default"],
+        # "moves": data["moves"][0],
     }
     return pokemon
 
 
 def fetch_pokemons(request):
-    pokemons = []
+    pokemons = request.session.get("pokemons")
+    if pokemons:
+        return render(request, "pokemons/fetch_pokemons.html", {"pokemons": pokemons, "title": "Fetch Pokemons"})
+    else:
+        pokemons = []
     for i in range(1, 26):
         pokemon = fetch_pokemon(i)
         pokemons.append(pokemon)
-    return render(request, 'pokemons/fetch_pokemons.html', {'pokemons': pokemons, 'title': 'Fetch Pokemons'})
+    request.session["pokemons"] = pokemons
+    return render(request, "pokemons/fetch_pokemons.html", {"pokemons": pokemons, "title": "Fetch Pokemons"})
 
 
 def add_feeding(request, pokemon_id):
@@ -156,43 +167,48 @@ def add_feeding(request, pokemon_id):
         new_feeding.pokemon_id = pokemon_id
         new_feeding.save()
         Pokemon.objects.get(id=pokemon_id).is_level_up()
-    return redirect('pokemon_detail', pokemon_id=pokemon_id)
+    return redirect("pokemon_detail", pokemon_id=pokemon_id)
 
 
 def signup(request):
-    error_message = ''
-    if request.method == 'POST':
+    error_message = ""
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')
+            return redirect("home")
         else:
-            error_message = 'Invalid sign up - try again'
+            error_message = "Invalid sign up - try again"
     form = UserCreationForm()
-    context = {'form': form, 'error_message': error_message}
-    return render(request, 'registration/signup.html', context)
+    context = {"form": form, "error_message": error_message}
+    return render(request, "registration/signup.html", context)
 
 
 def store(request):
-    url = 'https://pokeapi.co/api/v2/item-category/10/'
-    response = requests.get(url)
-    data = response.json()
-    items = []
-    for item in data['items']:
-        item_name = item['name']
-        item_url = item['url']
-        item = fetch_item(item_name, item_url)
-        if item != None:
-            items.append(item)
-    return render(request, 'store/store_view.html', {'title': 'Store', 'items': items})
+    items = request.session.get("session")
+    if items:
+        return render(request, "store/store_view.html", {"title": "Store", "items": items})
+    else:
+        url = "https://pokeapi.co/api/v2/item-category/10/"
+        response = requests.get(url)
+        data = response.json()
+        items = []
+        for item in data["items"]:
+            item_name = item["name"].lower()
+            item_url = item["url"]
+            item = fetch_item(item_name, item_url)
+            if item != None:
+                items.append(item)
+        request.session["session"] = items
+        return render(request, "store/store_view.html", {"title": "Store", "items": items})
 
 
 def fetch_item_img(name):
-    url = f'https://pokeapi.co/api/v2/item/{name}/'
+    url = f"https://pokeapi.co/api/v2/item/{name}/"
     response = requests.get(url)
     data = response.json()
-    img = data['sprites']['default']
+    img = data["sprites"]["default"]
     return img
 
 
@@ -200,21 +216,20 @@ def fetch_item(name, url):
     # https://pokeapi.co/api/v2/item/80/
     response = requests.get(url)
     data = response.json()
-    if data['cost'] and data['sprites']['default']:
-        print(name)
+    if data["cost"] and data["sprites"]["default"]:
         item = {
-            'name': name,
-            'cost': data['cost'],
-            'img': data['sprites']['default'],
+            "name": name,
+            "cost": data["cost"],
+            "img": data["sprites"]["default"],
         }
         return item
 
 
 def store_purchase(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         player = Player.objects.get(ownedby=request.user)
-        item_name = request.POST.get('name')
-        item_cost = request.POST.get('cost')
+        item_name = request.POST.get("name")
+        item_cost = request.POST.get("cost")
         # print(item_name, item_cost)
         try:
             item_cost = int(item_cost)
@@ -224,7 +239,7 @@ def store_purchase(request):
             player.money -= item_cost
             player.items[item_name] = player.items.get(item_name, 0) + 1
             player.save()
-    return redirect('/player/profile')
+    return redirect("/player/profile")
 
 
 class PlayerCreate(CreateView):
@@ -245,12 +260,11 @@ def player_profile(request):
         player = None
 
     if player == None:
-        return redirect('player_create')
+        return redirect("player_create")
     pokemons = Pokemon.objects.filter(ownedby=request.user)
     pokemons_count = pokemons.count()
     items = []
 
-    # print(player.items)
     for name, qty in player.items.items():
         item = {}
         item["name"] = name
@@ -259,5 +273,5 @@ def player_profile(request):
         items.append(item)
     pokemons_in_pocket = Pokemon.objects.filter(
         ownedby=request.user, in_pocket=True)
-    return render(request, 'player/player_profile.html', {'player': player,  'pokemons': pokemons, 'pokemon_count': pokemons_count,
-                                                          'pokemons_in_pocket': pokemons_in_pocket, 'title': 'Player Profile', 'items': items})
+    return render(request, "player/player_profile.html", {"player": player,  "pokemons": pokemons, "pokemon_count": pokemons_count,
+                                                          "pokemons_in_pocket": pokemons_in_pocket, "title": "Player Profile", "items": items})
