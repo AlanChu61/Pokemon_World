1b312bfa621830f7d6bfd3bf8b7be0651083c399
Alan.Chu <achu61@seas.upenn.edu>
2023-04-25 15:51:09 -0700

    font-sieze

--- pokemonworld/settings.py
+++ pokemonworld/settings.py
@@ -123,9 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
+STATIC_URL = "static/"
 LOGIN_REDIRECT_URL = "/pokemons/"
 LOGOUT_REDIRECT_URL = "/"
 # Default primary key field type
